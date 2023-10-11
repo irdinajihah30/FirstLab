eaf0142bbef78b6885362e9d1946cee77e5c4e81
irdinajihah30 <217764@student.upm.edu.my>
2023-10-11 20:53:48 +0800

    added Student.java class

---
+++ FirstLab/src/name/Student.java
@@ -0,0 +1,11 @@
+package name;
+
+public class Student {
+
+	public static void main(String[] args) {
+		System.out.println("WELCOME!\n"+"Name: Nurirdina Najihah\n"+"217764\n"
+	    +"I'm from Seri Kembangan, Selangor");
+
+	}
+
+}
